27e16784c1784b582583d7c988bcc0a8428b1e9a
Thanhvien04 <vienbui.798976@gmail.com>
2023-11-12 15:27:26 +0700

    vieneaaa

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'Screen/login_screen.dart';
 
+////vien
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
