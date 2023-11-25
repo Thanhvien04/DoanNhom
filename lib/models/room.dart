32e5cd7a60f1af7ba7e1b4d7f275f1ade8ea1faf
Lehuuthanh23 <thanhlebtre1245@gmail.com>
2023-11-25 15:50:35 +0700

    chuan bi merge

--- lib/models/room.dart
+++
@@ -1,8 +0,0 @@
-import 'package:doan/models/device.dart';
-
-class Room {
-  final String name;
-  List<Device> lstDevice;
-  final int id;
-  Room({required this.id, required this.lstDevice, required this.name});
-}
