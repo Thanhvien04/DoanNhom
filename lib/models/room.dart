50b82a6e4afa53d9f57aa2bd9a48d5a0bb69e2e6
Lehuuthanh23 <thanhlebtre1245@gmail.com>
2023-11-29 12:57:41 +0700

    delete room

--- lib/models/room.dart
+++ lib/models/room.dart
@@ -4,5 +4,6 @@ class Room {
   final String name;
   List<Device> lstDevice;
   final int id;
-  Room({required this.id, required this.lstDevice, required this.name});
+  final delete;
+  Room({required this.id, required this.lstDevice, required this.name, required this.delete});
 }
