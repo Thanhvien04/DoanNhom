8d051978080dea218cb46066216afc5d68c862ab
Congthanh22 <thanhpro22042003@gmail.com>
2023-12-01 16:24:51 +0700

    update setting

--- lib/Screen/profile_screen.dart
+++ lib/Screen/profile_screen.dart
@@ -19,6 +19,14 @@ class _ProfileState extends State<Profile_screen> {
   String? name = FirebaseAuth.instance.currentUser!.displayName;
   String? _phoneNumber = FirebaseAuth.instance.currentUser!.phoneNumber;
   String? _email = FirebaseAuth.instance.currentUser!.email;
+  void signOut() async {
+    try {
+      await auth.signOut();
+      print("Đăng xuất thành công");
+    } catch (e) {
+      print("Lỗi khi đăng xuất: $e");
+    }
+  }
 
   hexStringToColor(String hexColor) {
     hexColor = hexColor.toUpperCase().replaceAll("#", "");
@@ -101,17 +109,71 @@ class _ProfileState extends State<Profile_screen> {
                   //       fixedSize: Size(double.maxFinite, 50),
                   //     ),
                   //     child: Text(
-                  //       '$_password',
+                  //       '$_phoneNumber',
                   //       style: TextStyle(
                   //           color: Colors.white,
                   //           fontWeight: FontWeight.bold,
                   //           fontSize: 20),
                   //     )),
-                  const SizedBox(
-                    height: 50,
-                  ),
+                  // const SizedBox(
+                  //   height: 50,
+                  // ),
                   ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Row(children: [
+                                  Icon(
+                                    Icons.warning_amber,
+                                    color: Colors.redAccent,
+                                  ),
+                                  Text(
+                                    " Warning!",
+                                    style: TextStyle(
+                                        color: Colors.redAccent,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 25),
+                                  ),
+                                ]),
+                                content: const Text(
+                                  "Do you really want to be exported?",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15),
+                                ),
+                                actions: <Widget>[
+                                  Row(children: [
+                                    const Padding(padding: EdgeInsets.all(10)),
+                                    TextButton(
+                                        onPressed: () {
+                                          signOut();
+                                          Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      Login_screen()));
+                                        },
+                                        child: const Text(
+                                          "Ok",
+                                          style: TextStyle(color: Colors.black),
+                                        )),
+                                    const Padding(padding: EdgeInsets.all(25)),
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: const Text(
+                                        'Close',
+                                        style: TextStyle(color: Colors.black),
+                                      ),
+                                    ),
+                                  ])
+                                ],
+                              );
+                            });
+                      },
                       style: ElevatedButton.styleFrom(
                           fixedSize: const Size.fromHeight(50)),
                       child: const Text(
