9b781c48483137ac0a1700ff42b5be3211e86762
Lehuuthanh23 <thanhlebtre1245@gmail.com>
2023-12-04 10:31:12 +0700

    sửa tên app

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      initialRoute: '/',
+      initialRoute: '/login',
       routes: {
         '/': (context) => const Home_Screen(),
         '/forget': (context) => const ForgetPassword_Screen(),
